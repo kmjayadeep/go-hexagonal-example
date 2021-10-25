978cdd321dd0ac44ae4627cc378ea3f5cac0df48
Jayadeep KM <kmjayadeep@gmail.com>
2021-10-25 08:27:38 +0530

    add comments

--- shortner/model.go
+++ shortner/model.go
@@ -1,5 +1,6 @@
 package shortner
 
+// Redirect type contains the code, URL and the timestamp of when the object was Created
 type Redirect struct {
   Code string `json:"code" bson:"code" msgpack:"code"`
   URL string `json:"url" bson:"url" msgpack:"code" validate:"empty=false & format=url"`
