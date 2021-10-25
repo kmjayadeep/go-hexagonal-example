e4dc7038cdfe423b766ce4c5bb6eb05501a83292
Jayadeep KM <kmjayadeep@gmail.com>
2021-10-25 09:17:43 +0530

    add repository

---
+++ repository/memory/repository.go
@@ -0,0 +1,32 @@
+package memory
+
+import(
+  "github.com/kmjayadeep/go-hexagonal-example/shortner"
+  "github.com/pkg/errors"
+)
+
+type memoryRepository struct {
+  store map[string]shortner.Redirect
+}
+
+func newMemoryRepository() (*memoryRepository, error) {
+  store := make(map[string]shortner.Redirect)
+  return &memoryRepository{
+    store: store,
+  }, nil
+}
+
+func (m *memoryRepository) Find(code string) (*shortner.Redirect, error) {
+  r, ok := m.store[code]
+
+  if !ok {
+    return nil, errors.Wrap(shortner.ErrRedirectNotFound, "repository.Redirect.Find")
+  }
+
+  return &r, nil
+}
+
+func (m *memoryRepository) Store(redirect *shortner.Redirect) (error) {
+  m.store[redirect.Code] = *redirect
+  return nil
+}
